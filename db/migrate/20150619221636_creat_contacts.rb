4a839d7b05c5d5842a51b4866a38700bf7325f48
Jeff Wagar <j.wagar@live.com>
2015-06-19 22:30:24 +0000

    Added contacts table and model

---
+++ db/migrate/20150619221636_creat_contacts.rb
@@ -0,0 +1,10 @@
+class CreatContacts < ActiveRecord::Migration
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.string :email
+      t.string :comments
+      t.timestamp
+    end
+  end
+end
